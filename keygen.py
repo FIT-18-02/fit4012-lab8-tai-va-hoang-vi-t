da79b55f68dcf2e07cb7620ccd6cb0c4653adcfb
taivip2k6 <taibonxu10001@gmail.com>
2026-05-28 20:40:43 +0700

    Update keygen.py

--- keygen.py
+++ keygen.py
@@ -6,7 +6,7 @@ from secure_transfer_utils import generate_rsa_keypair
 PRIVATE_KEY_PATH = Path(os.getenv("RECEIVER_PRIVATE_KEY", "keys/receiver_private.pem"))
 PUBLIC_KEY_PATH = Path(os.getenv("RECEIVER_PUBLIC_KEY", "keys/receiver_public.pem"))
 
-
+ 
 def main() -> None:
     generate_rsa_keypair(PRIVATE_KEY_PATH, PUBLIC_KEY_PATH)
     print(f"[+] Đã tạo khóa riêng: {PRIVATE_KEY_PATH}")
